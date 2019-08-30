6d18c3121bd96116be5c8e3a012186d190168578
Alkihis <alkihis@gmail.com>
2019-08-30 15:45:27 +0200

    Types

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,8 @@
+import { TwitterArchive } from './Archive';
+
 export * from './Archive';
 export * from './Conversation';
 export * from './TwitterTypes';
 export * from './DMArchive';
+
+export default TwitterArchive;
